fc7e5fbb652ca5c8a856d6d8eed85caf5f8efdba
Meagan Williams <meaganwilliams407@gmail>
2022-06-10 13:03:02 +0200

    message

---
+++ code.js
@@ -0,0 +1,15 @@
+function smartPeople() {
+    console.log(smartPeople); 
+}
+
+let firstNames = [
+    'Michaela', 'Meagan',
+    'Charles', 'Liam', 
+    'Rasheedah', 'Bulelani'
+];
+
+let cnt = 0;
+do{
+    console.log(firstNames[cnt]);
+    cnt++;
+    }while(cnt < firstNames.length);
